d58de8091faf3317327aea81512d70622af7ad6a
kinopico <zqmillet@qq.com>
2022-09-14 21:42:04 +0800

    update __main__.py file.

--- crazy_thursday/__main__.py
+++ crazy_thursday/__main__.py
@@ -1,9 +1,13 @@
 from random import choice
+from os.path import dirname
+from os.path import join
+from json import loads
 
-from .articles import articles
+with open(join(dirname(__file__), 'corpus.jsonl'), 'r', encoding='utf8') as file:
+    lines = file.readlines()
 
 def main():
-    print(choice(articles))
+    print(loads(choice(lines))['body'])
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -5,20 +5,16 @@ this is the setup of this package.
 from setuptools import setup
 from setuptools import find_packages
 
-from crazy_thursday import VERSION
+with open('crazy_thursday/requirements.txt', 'r', encoding='utf8') as file:
+    install_requires = list(map(lambda x: x.strip(), file.readlines()))
 
 setup(
-    name='crazy_thursday',
-    version='.'.join(map(str, VERSION)),
+    name='crazy-thursday',
+    version='1.0.0',
     author='kinopico',
     author_email='zqmillet@qq.com',
-    url='https://github.com/zqmillet/crazy-thursday',
-    description='v me 50',
+    url='https://github.com/zqmillet/sphinx-console',
+    description='an extension for sphinx to display console in sphinx documents',
     packages=find_packages(),
-    entry_points = {
-        'console_scripts': [
-            'crazy-thursday = crazy_thursday.__main__:main',
-            'kfc = crazy_thursday.__main__:main',
-        ],
-    },
+    install_requires=install_requires,
 )
