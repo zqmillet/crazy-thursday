5797abf9c5087a44fe0d8ff255df6db8689e47cb
kinopico <zqmillet@qq.com>
2022-09-15 22:28:22 +0800

    [Debug] debug.

--- crazy_thursday/__main__.py
+++ crazy_thursday/__main__.py
@@ -2,6 +2,9 @@ from random import choice
 from os.path import dirname
 from os.path import join
 from json import loads
+from os import get_terminal_size
+
+columns = get_terminal_size().columns
 
 with open(join(dirname(__file__), 'corpus.jsonl'), 'r', encoding='utf8') as file:
     lines = file.readlines()
@@ -9,6 +12,8 @@ with open(join(dirname(__file__), 'corpus.jsonl'), 'r', encoding='utf8') as file
 def main():
     item = loads(choice(lines))
     print(item['body'] or item['title'])
+    print(('by ' + item['user']['name']).rjust(columns))
+    print(('at ' + item['created_at']).rjust(columns))
 
 if __name__ == '__main__':
     main()
