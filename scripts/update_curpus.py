77de30f774f39fd56bb497f9718d818742ed0f57
kinopico <zqmillet@qq.com>
2022-09-16 11:00:53 +0800

    [Debug] debug.

--- scripts/update_curpus.py
+++ scripts/update_curpus.py
@@ -7,9 +7,15 @@ from os.path import dirname
 from os.path import join
 from datetime import datetime
 from subprocess import call
+from pytz import timezone
 
 from crazy_thursday.get_issues import get_issues
 from crazy_thursday import VERSION
+from logging import DEBUG
+from logging import info
+from logging import basicConfig
+
+basicConfig(level=DEBUG)
 
 argument_parser = ArgumentParser()
 
@@ -44,7 +50,9 @@ with open(arguments.output_file_path, 'w', encoding='utf8') as file:
         file.write(issue.json(ensure_ascii=False) + '\n')
 
 year, month, day, build = VERSION
-today = datetime.today().date()
+now = datetime.now(timezone('Asia/Shanghai'))
+info('today is %s', now)
+today = now.date()
 publish_date = datetime(year, month, day).date()
 
 if today > publish_date:
@@ -54,3 +62,5 @@ else:
 
 with open(join(dirname(dirname(__file__)), 'crazy_thursday', '__init__.py'), 'w', encoding='utf8') as file:
     file.write(f'VERSION = ({today.year}, {today.month}, {today.day}, {build})')
+
+info(f'new version is {today.year}.{today.month}.{today.day}.{build}')
