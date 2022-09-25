98b499b2c66dad783d7f2d023a480e16c0f70d37
kinopico <zqmillet@qq.com>
2022-09-25 11:53:09 +0800

    [Debug] debug.

--- crazy_thursday/__main__.py
+++ crazy_thursday/__main__.py
@@ -1,22 +1,21 @@
 from random import choice
 from os.path import dirname
 from os.path import join
-from os import get_terminal_size
 from json import loads
 
-columns = get_terminal_size().columns
+from rich.console import Console
+from rich.markdown import Markdown
 
 with open(join(dirname(__file__), 'corpus.jsonl'), 'r', encoding='utf8') as file:
     lines = file.readlines()
 
-def main():
+def main() -> None:
     """
     this is the entry of this package.
     """
     item = loads(choice(lines))
-    print(item['body'] or item['title'])
-    print(('by ' + item['user']['name']).rjust(columns))
-    print(('at ' + item['created_at']).rjust(columns))
+    markdown = Markdown(f"{item['body']}\n\n- **create by**: {item['user']['name']}\n- **create at**: {item['created_at']}")
+    Console().print(markdown)
 
 if __name__ == '__main__':
     main()
