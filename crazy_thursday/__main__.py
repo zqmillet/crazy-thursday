104d20336217a2785cc4793ceeaaae6147dd3571
kinopico <zqmillet@qq.com>
2022-09-25 12:21:15 +0800

    [Debug] debug.

--- crazy_thursday/__main__.py
+++ crazy_thursday/__main__.py
@@ -5,6 +5,7 @@ from json import loads
 
 from rich.console import Console
 from rich.markdown import Markdown
+from rich.panel import Panel
 
 with open(join(dirname(__file__), 'corpus.jsonl'), 'r', encoding='utf8') as file:
     lines = file.readlines()
@@ -14,8 +15,13 @@ def main() -> None:
     this is the entry of this package.
     """
     item = loads(choice(lines))
-    markdown = Markdown(f"{item['body']}\n\n- **create by**: {item['user']['name']}\n- **create at**: {item['created_at']}")
-    Console().print(markdown)
+    Console().print(
+        Panel(
+            renderable=Markdown(item['body']),
+            subtitle=f"create by {item['user']['name']} at {item['created_at']}",
+            subtitle_align='right',
+        )
+    )
 
 if __name__ == '__main__':
     main()
